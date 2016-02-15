1d53bcd5f54aa82d4607c2081dec3a56e806ded4
Jeff Tulley <jefferson.tulley@mozy.com>
2016-02-15 13:14:54 -0700

    Fix donate button, js clock to deal with negative time, timezone

--- js/clock.js
+++ js/clock.js
@@ -6,6 +6,8 @@ function getTimeRemaining(endtime) {
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hoursOffset = new Date().getTimezoneOffset();
+
   return {
     'total': t,
     'days': days,
@@ -25,21 +27,30 @@ function initializeClock(id, endtime) {
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
+    if (t.total < 0) {
+      //clearInterval(timeinterval);
+      t.total = t.total;
+      t.days = 0 - t.days - 1;
+      t.hours = 0 - t.hours - 1;
+      t.minutes = 0 - t.minutes - 1;
+      t.seconds = 0 - t.seconds;
+      // change record breaking text to indicate the race is afoot
+      var introText = clock.querySelector('.introText');
+      introText.innerHTML = "RECORD ATTEMPT HAS BEEN GOING FOR";
+    }
+
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
   }
 
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(2016, 3, 22, 9, 0, 0, 0);
+var deadline = new Date(Date.UTC(2016, 3, 24, 13, 0, 0, 0));
 
 initializeClock('clockdiv', deadline);
 
