77faed43c291c310d84b7f413be4f081a4134943
Jeff Tulley <jefferson.tulley@mozy.com>
2016-02-22 23:53:42 -0700

    Changes required when moving the site to its real home
    There are still some .html files that could be updated,
    making absolute links on images.

--- theme/footer.php
+++ theme/footer.php
@@ -1,15 +1,15 @@
       <div class="row" style="padding: 0px;">
         <div class="col-xs-3 imagecontainer">
-           <a href="<?php echo site_url(); ?>?page_id=13"><img src="images/BottomBlockPicture1_225.png" alt="Cause"/></a>
+           <a href="<?php echo site_url(); ?>/the-cause/"><img src="/images/BottomBlockPicture1_225.png" alt="Cause"/></a>
         </div>
         <div class="col-xs-3 imagecontainer">
-           <a href="<?php echo site_url(); ?>?page_id=15"><img src="images/BottomBlockPicture2_225.png" alt="Crew"/></a>
+           <a href="<?php echo site_url(); ?>/crew/"><img src="/images/BottomBlockPicture2_225.png" alt="Crew"/></a>
         </div>
         <div class="col-xs-3 imagecontainer">
-           <a href="<?php echo site_url(); ?>?page_id=11"><img src="images/BottomBlockPicture3_225.png" alt="Lisa's Story"/></a>
+           <a href="<?php echo site_url(); ?>/lisas-story/"><img src="/images/BottomBlockPicture3_225.png" alt="Lisa's Story"/></a>
         </div>
         <div class="col-xs-3 imagecontainer">
-           <a href="<?php echo site_url(); ?>?page_id=21"><img src="images/BottomBlockPicture4_225.png" alt="Sponsorship"/></a>
+           <a href="<?php echo site_url(); ?>/sponsorship/"><img src="/images/BottomBlockPicture4_225.png" alt="Sponsorship"/></a>
         </div>
       </div>
       <div class="row">
@@ -18,16 +18,17 @@
           <p style="color: #2c7197; font-weight: bold">IT IS 100% ABOUT RAISING AWARENESS AND FUNDS</p>
           <p>for this cause so that they can continue saving the world.</p>
           <p>Any amount helps.  Thank you.</p>
-          <form class="form-inline" name="donate-form"><button type="submit" class="btn btn-primary center-block">DONATE</button></form>
+          <div class="topnavdonate"><a href="<?php echo site_url(); ?>/donate/"><button type="button" class="btn btn-primary center-block">DONATE</button></a></div>
+
         </div>
       </div>
 <div class="footerimage">
       <div class="row">
         <div class="col-xs-11 col-xs-offset-1 col-sm-offset-0 col-sm-3 footerlefttext">
           <p style="padding-left: 0px;">STAY CONNECTED</p>
-          <p><a href="https://www.facebook.com/RUNTheNation2016/"><img src="images/SocialMedia_Facebook_White.png" style="padding-right: 5px" alt="" />FACEBOOK</a></p>
-          <p><a href="https://twitter.com/RUN_The_Nation"><img src="images/SocialMedia_Twitter_White.png" style="padding-right: 5px" alt="" />TWITTER</a></p>
-          <p><a href="https://www.instagram.com/run_the_nation/"><img src="images/SocialMedia_Instagram_White.png" style="padding-right: 5px" alt="" />INSTAGRAM</a></p>
+          <p><a href="https://www.facebook.com/RUNTheNation2016/"><img src="/images/SocialMedia_Facebook_White.png" style="padding-right: 5px" alt="" />FACEBOOK</a></p>
+          <p><a href="https://twitter.com/RUN_The_Nation"><img src="/images/SocialMedia_Twitter_White.png" style="padding-right: 5px" alt="" />TWITTER</a></p>
+          <p><a href="https://www.instagram.com/run_the_nation/"><img src="/images/SocialMedia_Instagram_White.png" style="padding-right: 5px" alt="" />INSTAGRAM</a></p>
         </div>
         <div class="col-xs-12 col-sm-5 text-center footermiddletext">
           <div id="clockdiv">
@@ -51,11 +52,11 @@
           </div>
         </div>
         <div class="col-xs-11  col-xs-offset-1 col-sm-offset-1  col-sm-3 footerrighttext">
-          <p><a href="<?php echo site_url(); ?>?page_id=11">LISA'S STORY</a></p>
-          <p><a href="<?php echo site_url(); ?>?page_id=13">THE CAUSE</a></p>
-          <p><a href="http://runthenationtemp.birthritewc.com/?page_id=4">BLOG</a></p>
-          <p><a href="<?php echo site_url(); ?>?page_id=19">PRESS</a></p>
-          <p><a href="<?php echo site_url(); ?>?page_id=21">SPONSORSHIP</a></p>
+          <p><a href="<?php echo site_url(); ?>/lisas-story/">LISA'S STORY</a></p>
+          <p><a href="<?php echo site_url(); ?>/the-cause/">THE CAUSE</a></p>
+          <p><a href="<?php echo site_url(); ?>/?page_id=4">BLOG</a></p>
+          <p><a href="<?php echo site_url(); ?>/press/">PRESS</a></p>
+          <p><a href="<?php echo site_url(); ?>/sponsorship/">SPONSORSHIP</a></p>
         </div>
       </div>
       <div class="row">
@@ -69,7 +70,7 @@
 </div>
     </div>
   </div>
-  <script src="js/clock.js"></script>
+  <script src="/js/clock.js"></script>
   <?php wp_footer(); ?>
   </body>
 </html>
--- theme/header.php
+++ theme/header.php
@@ -5,7 +5,7 @@
     <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 		<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <link href="<?php echo site_url(); ?>/wp-content/themes/theme/style.css" rel="stylesheet" type="text/css" />
 
@@ -21,23 +21,24 @@
     <div class="topnav">
       <div class="row fullnavrow">
         <div class="col-xs-12 col-sm-3 navlogo">
-          <a href="<?php echo site_url(); ?>"><img src="images/RunTheNationLogo.png" alt="Run The Nation" style="padding: 0.5rem;"></a>
+          <a href="<?php echo site_url(); ?>"><img src="/images/RunTheNationLogo.png" alt="Run The Nation" style="padding: 0.5rem;"></a>
         </div>
         <div class="col-xs-12 col-sm-9 navitemsrow"> 
-          <span class="navitem"><a href="<?php echo site_url(); ?>?page_id=11">LISA'S STORY</a></span>
-          <span class="navitem"><a href="<?php echo site_url(); ?>?page_id=13">THE CAUSE</a></span>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/lisas-story/">LISA'S STORY</a></span>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/the-cause/">THE CAUSE</a></span>
           <span class="navitem" style="padding:0px 0px 0px 10px;"><div class="dropdown"><span>THE RUN<span class="dropdownarrow"> &#9660;</span></span>
            <div class="dropdown-content">
-            <p><a href="<?php echo site_url(); ?>?page_id=15">CREW</a></p>
-            <p><a href="<?php echo site_url(); ?>?page_id=17">COURSE</a></p>
-            <p><a href="http://runthenationtemp.birthritewc.com/?page_id=4&page_type=video">LIVE&nbsp;FEED</a></p>
+            <p><a href="<?php echo site_url(); ?>/crew/">CREW</a></p>
+            <p><a href="<?php echo site_url(); ?>/course/">COURSE</a></p>
+            <p><a href="<?php echo site_url(); ?>/blog/?page_type=video">LIVE&nbsp;FEED</a></p>
             </div>
             </div>
           </span>
-          <span class="navitem"><a href="http://runthenationtemp.birthritewc.com/?page_id=21">SPONSORSHIP</a></span>
-          <span class="navitem"><a href="http://runthenationtemp.birthritewc.com/?page_id=4">BLOG</a></span>
-          <span class="navitem"><a href="<?php echo site_url(); ?>?page_id=19">PRESS</a></span>
-          <div class="topnavdonate"><form class="form-inline" name="donate-form"><button type="submit" class="btn btn-primary center-block">DONATE</button></form></div>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/sponsorship/</a></span>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/blog/">BLOG</a></span>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/press/">PRESS</a></span>
+          <span class="navitem"><a href="<?php echo site_url(); ?>/contact-us/">CONTACT US</a></span>
+          <div class="topnavdonate"><a href="<?php echo site_url(); ?>/donate/"><button type="button" class="btn btn-primary center-block">DONATE</button></a></div>
         </div>
       </div>
     </div>
