6cabc3795734f863f697832a8b9aef4ae21d11e1
Jeff Tulley <jefferson.tulley@mozy.com>
2016-03-04 10:42:58 -0700

    Top countdown clock; new sponsor(DC), better donation page

--- js/clock.js
+++ js/clock.js
@@ -17,41 +17,47 @@ function getTimeRemaining(endtime) {
   };
 }
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+function initializeClock(id, endtime, centerClockDiv) {
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
+  function updateClock(id, endtime, centerClockDiv) {
+    var clock = document.getElementById(id);
+    if (clock != undefined) {
+    	var daysSpan = clock.querySelector('.days');
+    	var hoursSpan = clock.querySelector('.hours');
+    	var minutesSpan = clock.querySelector('.minutes');
+    	var secondsSpan = clock.querySelector('.seconds');
 
-    if (t.total < 0) {
-      //clearInterval(timeinterval);
-      t.total = t.total;
-      t.days = 0 - t.days - 1;
-      t.hours = 0 - t.hours - 1;
-      t.minutes = 0 - t.minutes - 1;
-      t.seconds = 0 - t.seconds;
-      // change record breaking text to indicate the race is afoot
-      var introText = clock.querySelector('.introText');
-      introText.innerHTML = "RECORD ATTEMPT HAS BEEN GOING FOR";
+    	var t = getTimeRemaining(endtime);
+
+    	if (t.total < 0) {
+      	//clearInterval(timeinterval);
+      	t.total = t.total;
+      	t.days = 0 - t.days - 1;
+      	t.hours = 0 - t.hours - 1;
+      	t.minutes = 0 - t.minutes - 1;
+      	t.seconds = 0 - t.seconds;
+      	// change record breaking text to indicate the race is afoot
+      	var introText = clock.querySelector('.introText');
+      	introText.innerHTML = "RECORD ATTEMPT HAS BEEN GOING FOR";
+    	}
+
+    	daysSpan.innerHTML = t.days;
+    	hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    	minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    	secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    	if (centerClockDiv === true) {
+  			alert("would center");
+  			centerClockDiv = false;
+			}
     }
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
   }
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  updateClock(id, endtime, centerClockDiv);
+  var timeinterval = setInterval(function() { updateClock(id, endtime, centerClockDiv); }, 1000);
 }
 
 var deadline = new Date(Date.UTC(2016, 3, 24, 13, 0, 0, 0));
-
-initializeClock('clockdiv', deadline);
+initializeClock('topclockdiv', deadline, false);
+initializeClock('clockdiv', deadline, false);
 
 }());
