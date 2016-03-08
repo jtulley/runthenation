6314c033d82b69ded5bc0112ece48e9441fecefa
Jeff Tulley <jefferson.tulley@mozy.com>
2016-03-07 20:51:46 -0700

    Full-width site, not limited by 900px
    all local html pages updated with current header and footer.

---
+++ js/navrow.js
@@ -0,0 +1,11 @@
+toggleNavItemsDisplay = function ()
+{
+  var navrow = document.getElementById('navitemsrow');
+  if (navrow.style.display === "block")
+  {
+    navrow.style.display="none";
+  } else
+  {
+    navrow.style.display="block";
+  }
+}
